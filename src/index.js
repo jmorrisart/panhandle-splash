10ffa02c09d08cf0608fe804ce358d51dc97d082
jmorrisart <morris.jess26@gmail.com>
2023-01-26 07:10:24 -0700

    style and content

--- src/index.js
+++ src/index.js
@@ -4,9 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import PanhandleLogo from './assets/images/logoBW.png';
+import BlowdryImg from './assets/images/blowdry.JPG';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>hi</h1>
+  <div className="container-all">
+
+    <img src={ PanhandleLogo } />
+    <h1>Our website is getting a makeover!</h1>
+    <hr />
+
+    <div className="text-container">
+      <p>Don't worry, you can still contact us to schedule an appointment. 
+        Please give us a call at <span style={{fontWeight: 700}}>(509) 447-2176</span> so we can get you taken care of!
+        In the meantime, don't forget to follow us on <a href="https://www.instagram.com/panhandlestudio/?hl=en">Instagram!</a>
+      </p>
+    </div>
+
+    <div className="image-container">
+      <img className="blowdry" src={ BlowdryImg } />
+    </div>
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
